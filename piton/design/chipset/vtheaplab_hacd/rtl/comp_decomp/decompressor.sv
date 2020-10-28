475582cfa7e544ad912cb7724bdf7860a7bf962c
Chandler Jearls <cjearls@localhost.localdomain>
2020-10-28 15:51:11 -0400

    fixing another typo

--- piton/design/chipset/vtheaplab_hacd/rtl/comp_decomp/decompressor.sv
+++ piton/design/chipset/vtheaplab_hacd/rtl/comp_decomp/decompressor.sv
@@ -37,7 +37,7 @@ huffmanDecompressorCacheLineWrapper decomp(
 	.io_readValid(rd_valid),
 	.io_writeFifoFull(wrfifo_full),
 	.io_writeRequest(wr_req),
-	.io_writedata(wr_data),
+	.io_writeData(wr_data),
 	.io_done(comp_done)
 );
 
